4bc0aaf4598af17b60aec42bd0e229f3ce520a66
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-11-24 09:52:51 +0000

    add middleware for cookies

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module MyInterestBackendA
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    # config.session_store :cookie_store, key: '_session', same_site: :strict
+    # config.session_store :cookie_store, key: '_session'
 
 
     # Settings in config/environments/* take precedence over those specified here.
@@ -36,8 +36,12 @@ module MyInterestBackendA
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore, key: '_session'
+    config.middleware.use ActionDispatch::Session::CookieStore
     # config.middleware.use ActionDispatch::Session::CookieStore, key: '_cookie_name'
+    # , same_site: :none, secure: :true
+    # config.middleware.use ActionDispatch::Cookies
+    # config.middleware.use ActionDispatch::Session::CookieStore
+    # config.middleware.insert_after(ActionDispatch::Cookies, ActionDispatch::Session::CookieStore)
 
   end
 end

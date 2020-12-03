e63fe40bb7b93914fc1c2068f2bd7694881cee28
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-12-03 18:10:02 +0000

    add create action

--- app/controllers/api/v1/boards_controller.rb
+++ app/controllers/api/v1/boards_controller.rb
@@ -3,4 +3,20 @@ class Api::V1::BoardsController < ApplicationController
         boards = Board.all
         render json: BoardSerializer.new(boards)
     end
+
+    def create
+        # byebug
+        @board = current_user.boards.build(board_params)
+    
+        if @board.save
+          render json:  BoardSerializer.new(@board), status: :created
+        else
+          error_resp = {
+            error: @board.errors.full_messages.to_sentence
+          }
+          render json: error_resp, status: :unprocessable_entity
+        end
+      end
+
+
 end

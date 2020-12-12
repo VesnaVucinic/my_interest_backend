b22ef2f120682fe0a2fd6dac5a2663c68a30fbe8
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-12-12 23:50:29 +0000

    add index action for logged_in and not logged_in user

--- app/controllers/api/v1/boards_controller.rb
+++ app/controllers/api/v1/boards_controller.rb
@@ -1,11 +1,40 @@
 class Api::V1::BoardsController < ApplicationController
-    def index
-        boards = Board.all
-        render json: BoardSerializer.new(boards)
+ 
+  def index
+        # byebug
+        if logged_in?
+            @boards = current_user.boards
+            render json:  BoardSerializer.new(@boards), status: :ok 
+        else 
+            @boards = Boards.all
+            render json: BoardSerializer.new(@boards), status: :ok 
+            # render json: {
+            #     error: "You must be logged in to see boards"
+            # }
+        end
     end
 
+    # def index
+    #   # byebug
+    #     @boards = Board.all
+    #     render json: BoardSerializer.new(@boards) 
+    # end
+
+  #   def index 
+  #     if  params[:user_id]
+  #         @user = User.find_by_id(params[:user_id])
+  #         @boards = @user.boards 
+  #         render json:  BoardSerializer.new(boards), status: :ok 
+  #     else
+  #     #if it's not nested: /reviews
+  #         @boards = Boards.all
+  #         render json:  BoardSerializer.new(boards), status: :ok
+  #     end
+  # end
+
+
     def create
-        # byebug
+      # byebug
         @board = current_user.boards.build(board_params)
     
         if @board.save
@@ -18,5 +47,13 @@ class Api::V1::BoardsController < ApplicationController
         end
       end
 
+      private
 
+      def set_user
+        @user = User.find(params[:user_id])
+      end
+
+      def board_params
+        params.require(:board).permit(:name, :image_url, :user_id, :id)
+      end
 end
